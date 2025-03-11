3e7e4988814288e4f7b9d7869b38f07753369d6c
atyamaru <ayaka111211@gmail.com>
2025-03-11 09:21:46 +0900

    thanks完了

---
+++ src/resources/views/thanks.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thanks-form</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/thanks.css') }}" />
+    <!--Inikaのフォント-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inika&display=swap" rel="stylesheet">
+</head>
+<body>
+    <main>
+        <div class="thanks__content">
+            <div class="thanks__heading">
+                <h2>お問い合わせありがとうございました</h2>
+            </div>
+            <div class="thanks__button">
+                <a class="thanks__button-home" href="/contact">HOME</a>
+            </div>
+            <p class="thanks__background">Thank you</p>
+        </div>
+    </main>
+</body>
+</html>
